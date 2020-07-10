b00989c0f51c7146c41cbfe3c717d21f2a0a2946
Jan Pikl <pikl.jan@gmail.com>
2020-07-10 23:16:14 +0200

    Store input in location.hash

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import debounce from 'debounce';
 import createEditor from './createEditor';
 import Evaluator from './Evaluator';
 import OutputRenderer from './OutputRenderer';
-import StorageWrapper from './StorageWrapper';
+import UrlHashStorage from './UrlHashStorage';
 
 const editorElement = document.getElementById('editor');
 const outputElement = document.getElementById('output');
@@ -12,20 +12,20 @@ const intro = fs.readFileSync(__dirname + '/intro.js', 'utf8');
 const evaluator = new Evaluator({timeout: 500});
 const editor = createEditor(editorElement);
 const renderer = new OutputRenderer(outputElement);
-const storage = new StorageWrapper(localStorage);
+const storage = new UrlHashStorage();
 
 editor.on('change', debounce(() => {
+  storage.set(editor.getValue());
   evaluator.post(editor.getValue());
 }, 500));
 
 evaluator.on('success', (input, output) => {
   renderer.renderOutput(output);
-  storage.saveInput(input);
 });
 
 evaluator.on('error', error => {
   renderer.renderError(error);
 });
 
-editor.setValue(storage.loadInput() || intro);
+editor.setValue(storage.get() || intro);
 editor.focus();
